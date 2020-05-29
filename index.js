6fabe2b847ffb314ca8e46520afb4e4049f322bc
Wippit.it <vanha.jungle@gmail.com>
2020-05-29 16:59:12 +0700

    change mongoDB to mongodb in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const mongodb = require("mongoDB");
+const mongodb = require("mongodb");
 // getting-started.js
 var mongoose = require('mongoose');
 mongoose.connect( process.env.MONGO_URL, 
